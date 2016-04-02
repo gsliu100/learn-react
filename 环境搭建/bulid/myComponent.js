b567e7422f825d1fdfaba5ce79d0877fb2866553
gsliu100 <gsliu100@gmail.com>
2016-04-02 12:44:45 +0800

    删除myComponent中的没必要的组件
    删除myComponent中的没必要的组件

--- 环境搭建/bulid/myComponent.js
+++ 环境搭建/bulid/myComponent.js
@@ -13,19 +13,5 @@ var CommentBox = React.createClass({
   }
 });
 
-// string style component
-var sComponent = React.createElement(
-  "h3",
-  { className: "commentBox" },
-  "Hello react sComponent "
-);
-// 上边两种创建组件的方式在react内部都是通过createElement方法穿件的
-var sb = React.createElement('h3', { className: 'commentBox', name: 'sb' }, 'Hello react sb');
 
-var myCommnentBox = ReactDOM.render(React.createElement(CommentBox, { name: "liuguangsheng" }), document.getElementById('myContainer'));
-
-ReactDOM.render(sComponent, document.getElementById('myContainer1'));
-
-ReactDOM.render(sb, document.getElementById('myContainer2'));
-
-console.log(myCommnentBox.setProps({ name: 'setliu' }));
+ReactDOM.render(React.createElement(CommentBox, { name: "liuguangsheng" }), document.getElementById('myContainer'));
--- 环境搭建/js/myComponent.js
+++ 环境搭建/js/myComponent.js
@@ -9,25 +9,8 @@ var CommentBox = React.createClass({
   }
 });
 
-// string style component
-var sComponent = <h3 className="commentBox">Hello react sComponent </h3>;
-// 上边两种创建组件的方式在react内部都是通过createElement方法穿件的
-var sb = React.createElement('h3',{className:'commentBox',name:'sb'},'Hello react sb');
 
-var myCommnentBox = ReactDOM.render(
+ReactDOM.render(
   <CommentBox name="liuguangsheng"></CommentBox>,
   document.getElementById('myContainer')
 );
-
-ReactDOM.render(
-  sComponent,
-  document.getElementById('myContainer1')
-);
-
-ReactDOM.render(
-  sb,
-  document.getElementById('myContainer2')
-);
-
-
-console.log(myCommnentBox.setProps({name:'setliu'}));
